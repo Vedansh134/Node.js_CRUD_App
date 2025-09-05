eca90f15bccfd12f55de43e53eeb82d510df3faf
Vedansh kumar <kumarvedansh134@gmail.com>
2025-09-05 12:51:03 +0530

    Create separate files for docker & docker compose

--- docker-install.sh
+++ docker-install.sh
@@ -39,19 +39,5 @@ $SUDO systemctl is-active docker
 echo "install git and tree"
 $SUDO apt-get install git tree -y
 
-# docker compose installation
-sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-
-# Install Docker Compose
-sudo apt install docker-compose -y
-
-# Apply executable permissions to the binary
-sudo chmod +x /usr/local/bin/docker-compose
-
-# Check Docker Compose version
-echo " 📦 Checking Docker Compose version..."
-docker-compose version
-
 
 # ===================== end of script =============================
-
